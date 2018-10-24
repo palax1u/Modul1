e69b390cb90db5c87b73638bba8ba63e32ce97c5
palax1u <palax1u@gmail.com>
2018-10-24 15:25:56 +0700

    Modul 1
    Tugas Modul 1 Kel 35

---
+++ Kelompok35/Kelompok35/Kelompok35/NewFolder/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kelompok35.NewFolder
+{
+    class User
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public string Email { get; set; }
+
+    }
+}
---
+++ Kelompok35/Kelompok35/Kelompok35/NewFolder/UserLogin.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kelompok35.NewFolder
+{
+    class UserLogin
+    {
+        public static string Username = "Kelompok35";
+        public static string Password = "password";
+
+    }
+}
